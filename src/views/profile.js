5d83646fcca3fb181ca7551b28b9178605a9b799
wasupon <wasupon@busx.com>
2020-10-14 01:13:33 +0700

    system setting

--- src/views/profile.js
+++ src/views/profile.js
@@ -20,14 +20,79 @@ export default {
         "Web Developer",
         "FullStack Developer",
     ],
+    certificates: [
+        {
+            title: "Certificate ITPE<br>IP",
+            category: "สวทช.",
+            images: [
+                "/images/profile/itpe1.jpg",
+                "/images/profile/itpe2.jpg",
+            ],
+        },
+        {
+            title: "Certificate TPQI<br>นักพัฒนาระบบชั้น 3",
+            category: "สถาบันคุณวุฒิวิชาชีพ",
+            images: [
+                "/images/profile/tpqi1.jpg",
+                "/images/profile/tpqi2.jpg",
+            ],
+        },
+        {
+            title: "Certificate IC3",
+            category: "สถาบันคุณวุฒิวิชาชีพ",
+            images: [
+                "/images/profile/ic3.jpg",
+            ],
+        },
+    ],
+    awards: {
+        fullname: "Peerapat Matheang",
+        images: "/images/profile/wewebplus-05971-172x172.jpg",
+        work: "FullStack Developer",
+        desc: "All awards received since college.",
+        list: [
+            {
+                title: "04 September 2016",
+                desc: "ACM ICPC 2016 รองชนะเลิศอันดับ 1 ระดับภูมิภาคเหนือ",
+            },
+            {
+                title: "17 - 18 October 201",
+                desc: "ACM ICPC 2016 ระดับประเทศ Thailand National On-site",
+            },
+            {
+                title: "23 June 2017",
+                desc: "Startup Thailand 2017",
+            },
+            {
+                title: "01 April 2018",
+                desc: "Startup Thailand 2018",
+            },
+            {
+                title: "05 - 07 May 2017",
+                desc: "NDX-Northern Digital Expo 2017",
+            },
+            {
+                title: "06 February 2017",
+                desc: "การแข่งขันพัฒนาโปรแกรมคอมพิวเตอร์แห่งประเทศไทย ครั้งที่ 19 (NSC 2017)",
+            },
+            {
+                title: "14 February 2018",
+                desc: "การแข่งขันพัฒนาโปรแกรมคอมพิวเตอร์แห่งประเทศไทย ครั้งที่ 20 (NSC 2018)",
+            },
+            {
+                title: "30 November 2016",
+                desc: "UP Disrupt 2016",
+            },
+        ],
+    },
     resume: {
         experiences: [
             {
                 isActive: true,
                 year: "2020 - Present",
                 subject: "FullStack Developer",
-                title: "BusX Co,. Ltd.",
-                desc: "Is the largest ticketing platform website development company in Thailand.",
+                title: "289 Production House Co,. Ltd.",
+                desc: "It is a web development company with government clients and private companies.",
             },
             {
                 isActive: true,
@@ -61,15 +126,15 @@ export default {
         },
         {
             title: "Always seeking new knowledge",
-            scale: "70%",
+            scale: "90%",
         },
         {
             title: "Reasoning and Problem Solving",
-            scale: "80%",
+            scale: "85%",
         },
         {
             title: "Enthusiasm for work",
-            scale: "90%",
+            scale: "75%",
         },
     ],
     languages: [
